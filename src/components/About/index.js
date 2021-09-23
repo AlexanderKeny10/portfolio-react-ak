46b9783658bbe9b900904f3254dfbaad54eb922b
AKen00 <ajken04@gmail.com>
2021-09-23 18:53:25 -0400

    new resume Link and about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,12 +8,13 @@ function About() {
             <div className='bio-container'>
                 <img className='alex-pic' src={pic} alt='Alexander Kenney' />
                 <p className='bio'>
-                JavaScript Developer with 4+ years in Multimedia Production, Marketing, and Web Design.
-                Currently holds a degree in Media Studies from Temple University and professional certificates in Web Development. 
-                Passionate about leveraging experience in multimedia production with technical background in JavaScript, React, and MySQL to develop web applications for a mission driven company. 
-                Exposure to recording and editing visual information, graphic technology manipulation, sound recording, and post-production technical editing as well. 
-                Expect to apply my acquired skills in the fast growing coding field. 
-            </p>
+                JavaScript Developer with 4+ years in Multimedia Production, Marketing, and Web Design. Currently holds a degree in
+                Media Studies from Temple University and professional certificates in Web Development. Passionate about leveraging
+                experience in multimedia production with technical background in JavaScript, React, and MySQL to develop web
+                applications for a mission driven company. Exposure to using visual and audio software has led me down the path of
+                building and designing web applications for my own passions and interests. Expect to apply my acquired skills in the fast-
+                growing coding field.
+                </p>
             </div>
         </section>
     );
