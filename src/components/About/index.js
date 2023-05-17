5ccbbf3751aa446dae8e311d5f66d333a2bb9153
AKen00 <ajken04@gmail.com>
2023-05-17 09:31:09 -0400

    Updated About Paragraph

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,12 +8,10 @@ function About() {
             <div className='bio-container'>
                 <img className='alex-pic' src={pic} alt='Alexander Kenney' />
                 <p className='bio'>
-                JavaScript Developer with 4+ years in Multimedia Production, Marketing, and Web Design. Currently holds a degree in
-                Media Studies from Temple University and professional certificates in Web Development. Passionate about leveraging
-                experience in multimedia production with technical background in JavaScript, React, and MySQL to develop web
-                applications for a mission driven company. Exposure to using visual and audio software has led me down the path of
-                building and designing web applications for my own passions and interests. Expect to apply my acquired skills in the fast-
-                growing coding field.
+                JavaScript Developer with 4+ years in Multimedia Production, Marketing, and Web Design. Currently holds a degree in 
+                Media Studies from Temple University and professional certificates in Full-Stack Web Development from University of Pennsylvania. 
+                Passionate about leveraging experience in multimedia production with technical background in JavaScript, React, Anguar and RESTful API’s 
+                to develop web applications for a mission driven company. Expect to apply my acquired skills in the fast growing coding field. 
                 </p>
             </div>
         </section>
