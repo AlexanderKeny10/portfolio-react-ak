061e0bca4ca3fe70cb4701a2b24a7ae68b628942
AKen00 <ajken04@gmail.com>
2024-02-27 11:26:50 -0500

    update About page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,10 +8,11 @@ function About() {
             <div className='bio-container'>
                 <img className='alex-pic' src={pic} alt='Alexander Kenney' />
                 <p className='bio'>
-                JavaScript Developer with 4+ years in Multimedia Production and 2+ years in Web Development and Design. and Web Design. 
-                Currently holds a degree in Media Studies from Temple University and professional certificates in Full-Stack Web Development from University of Pennsylvania. 
-                Passionate about leveraging experience in multimedia production with technical background in JavaScript, React, Anguar and RESTful API’s 
-                to develop web applications for a mission driven company. Expect to apply my acquired skills in the fast growing coding field. 
+                Full-Stack Developer with 5 years in Multimedia Production and 3 years in Web Development and and Relational Data Management. 
+                Currently holds a Batchelor degree in Communications from Temple University and professional certificates in Full-Stack Web Development from University of Pennsylvania. 
+                Passionate about leveraging experience in production with technical background in HTML, CSS, JavaScript, Python, 
+                MySQL Relational Database and RESTful API’s to help develop web applications for a mission driven company.
+                Expect to apply my acquired skills in the fast growing coding field. 
                 </p>
             </div>
         </section>
